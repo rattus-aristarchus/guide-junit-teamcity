ed6694dcbdd2bed94b7ba74b43be3e8a1081fcbe
rattus-aristarchus <lankin.m.a@gmail.com>
2024-07-25 16:34:22 +0300

    the project

---
+++ src/test/java/org/allure/junit5_jenkins/SelenideTest.java
@@ -0,0 +1,38 @@
+package org.allure.junit5_jenkins;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.Allure;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SelenideTest {
+
+
+    @BeforeAll
+    static void setupAllureReports() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @BeforeAll
+    static void setupSelenoid() {
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+    }
+
+    @Test
+    public void testMainPage() {
+        Allure.step("Open main page", (step) -> {
+            open("https://www.saucedemo.com");
+        });
+
+        Allure.step("The login logo should be visible", (step) -> {
+            $(".login_logo").shouldBe(Condition.visible);
+        });
+    }
+
+}
